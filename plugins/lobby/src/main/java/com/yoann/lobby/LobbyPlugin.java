568c852991914bbac6823e3ee55bfbdf07a243b4
PsykoEtnc <hibouxtoumou@gmail.com>
2025-10-05 14:43:46 +0200

    Update LobbyPlugin.java

--- plugins/lobby/src/main/java/com/yoann/lobby/LobbyPlugin.java
+++ plugins/lobby/src/main/java/com/yoann/lobby/LobbyPlugin.java
@@ -4,7 +4,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.command.Command;
-import org.bukkit.CommandSender;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
